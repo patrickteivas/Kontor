db269e212c3ee7a04cf614205d222b991fd85bf0
patrickteivas <teivas.2011@mail.ru>
2018-02-08 09:36:43 +0200

    Base commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /*
+            Meil on maja/kontor kus töötajad peaksid saama ligipääsu erinevatele ruumidele. 
+            Ruumideks on: arendajate ruum, ülemuste ruum, köök, andmetöötlejate ruum.
+            Töötajad jagunevad gruppidesse: Nooremarendajad, vanemarendajad, koristajad, spetsiaalkoristaja, ülemused, andmetöötlejad.
+
+            Peauksest peaks saama sisse iga töötaja. Arendajate ruumi peaks sisse saama kõik peale andmetöötlejate. Ülemuste ruumi peaks sisse saama vanemarendajad,
+            spetsiaalkoristajad ja ülemused. Kööki peaks kõik sisse saama. Andmetöötlejate ruumi peaks kõik peale nooremarendajate sisse saama.
+            
+            Kasutada tuleb objektorienteeritust - pärilikkus, kapseldamine, polümorfism.
+            */
+
+            new Nooremarendaja().Amet();
         }
     }
 }
---
+++ ConsoleApp1/Töötaja.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Töötaja
+    {
+        public string Name;
+        protected bool PeauksAccess = true;
+        protected bool ArendajateRuumAccess = false;
+        protected bool ÜlemusteRuumAccess = false;
+        protected bool KöökAccess = true;
+        protected bool AndmetöötlejateRuumAccess = false;
+
+        public void Amet()
+        {
+            Console.WriteLine("Teie amet on " + Name);
+        }
+    }
+
+    class Nooremarendaja : Töötaja
+    {
+        public Nooremarendaja()
+        {
+            Name = "Nooremarendaja";
+        }
+
+        public void Amet()
+        {
+            base.Amet();
+        }
+    }
+
+    class Vanemarendaja : Töötaja
+    {
+        public Vanemarendaja()
+        {
+            Name = "Vanemarendaja";
+        }
+    }
+
+    class Koristaja : Töötaja
+    {
+        public Koristaja()
+        {
+            Name = "Koristaja";
+        }
+    }
+
+    class Spetsiaalkoristaja : Töötaja
+    {
+        public Spetsiaalkoristaja()
+        {
+            Name = "Spetsiaalkoristaja";
+        }
+    }
+
+    class Ülemused : Töötaja
+    {
+        public Ülemused()
+        {
+            Name = "Ülemused";
+        }
+    }
+
+    class Andmetöötleja : Töötaja
+    {
+        public Andmetöötleja()
+        {
+            Name = "Andmetöötleja";
+        }
+    }
+}
